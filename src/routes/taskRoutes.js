175c05296572fd47a72551705cfd8f5dba59b87f
anubhavsultania <122CH0539@nitrkl.ac.in>
2026-07-10 23:04:47 +0530

    feat: refactor taskController

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,151 +1,71 @@
 import express from "express";
-import db from "../db.js";
+
+import { isAuthenticated } from "../middleware/auth.js";
+import { validate } from "../middleware/validate.js";
+
+import * as taskControllers from "../controllers/taskController.js";
+
 import {
   idSchema,
   titleSchema,
+  createTaskSchema,
+  moveTaskSchema,
+  completeTaskSchema,
   getTasksSchema,
 } from "../validators/taskValidators.js";
-import { validate } from "../middleware/validate.js";
-import { isAuthenticated } from "../middleware/auth.js";
-import {
-  getTasks,
-  searchTasks,
-  moveTask,
-} from "../controllers/taskController.js";
 
 const router = express.Router();
 
-router.get("/", isAuthenticated, validate(getTasksSchema, "query"), getTasks);
 router.get(
-  "/search",
+  "/",
   isAuthenticated,
-  validate(titleSchema, "query"),
-  searchTasks,
+  validate(getTasksSchema, "query"),
+  taskControllers.getTasks,
 );
-router.get("/:id", isAuthenticated, (req, res) => {
-  const taskId = req.params.id;
-  const userId = req.session.userId;
-  db.get(
-    `SELECT * FROM tasks
-        WHERE user_id = ?
-        AND id = ?
-    `,
-    [userId, taskId],
-    (err, row) => {
-      if (err) {
-        return res.status(500).send(err.message);
-      } else if (!row) {
-        return res.status(404).send("Task not found");
-      }
-      return res.json(row);
-    },
-  );
-});
 
-router.post("/", isAuthenticated, (req, res) => {
-  const userId = req.session.userId;
-  const { title } = req.body;
-  if (!title || title.trim() === "") {
-    return res.status(400).send("Title is required");
-  }
-  db.run(
-    `INSERT INTO tasks (title, user_id)
-        VALUES (?, ?)`,
-    [title, userId],
-    (err) => {
-      if (err) {
-        return res.status(500).send(err.message);
-      }
-      return res.status(201).json({
-        message: "Task Created",
-      });
-    },
-  );
-});
+router.get(
+  "/:id",
+  isAuthenticated,
+  validate(idSchema, "params"),
+  taskControllers.getTask,
+);
 
-router.patch("/:id", isAuthenticated, (req, res) => {
-  const userId = req.session.userId;
-  const { title } = req.body;
-  const taskId = req.params.id;
-  if (!title || title.trim() === "") {
-    return res.status(400).send("Title is required");
-  }
-  db.run(
-    `UPDATE tasks
-            SET title = ?
-            WHERE user_id = ?
-            AND id = ?
-    `,
-    [title, userId, taskId],
-    function (err) {
-      if (err) {
-        return res.status(500).send(err.message);
-      }
-      if (this.changes === 0) {
-        return res.status(404).send("Task not found");
-      }
-      return res.status(201).json({
-        message: "Task Created",
-      });
-    },
-  );
-});
+router.post(
+  "/",
+  isAuthenticated,
+  validate(createTaskSchema, "body"),
+  taskControllers.createTask,
+);
 
-router.delete("/:id", isAuthenticated, (req, res) => {
-  const taskId = req.params.id;
-  const userId = req.session.userId;
+router.patch(
+  "/:id",
+  isAuthenticated,
+  validate(idSchema, "params"),
+  validate(titleSchema, "body"),
+  taskControllers.renameTask,
+);
 
-  db.run(
-    `
-        DELETE FROM tasks
-        WHERE user_id = ?
-        AND id = ?
-    `,
-    [userId, taskId],
-    function (err) {
-      if (err) {
-        return res.status(500).send(err.message);
-      }
-      if (this.changes === 0) {
-        return res.status(404).send("Task not found");
-      }
-      return res.json({
-        message: "Task deleted",
-      });
-    },
-  );
-});
+router.delete(
+  "/:id",
+  isAuthenticated,
+  validate(idSchema, "params"),
+  taskControllers.deleteTask,
+);
 
-router.patch("/:id/complete", isAuthenticated, (req, res) => {
-  const userId = req.session.userId;
-  const taskId = req.params.id;
-
-  db.run(
-    `UPDATE tasks
-        SET completed = 1
-        WHERE user_id = ?
-        AND id = ?
-    `,
-    [userId, taskId],
-    function (err) {
-      if (err) {
-        return res.status(500).send(err.message);
-      }
-      if (this.changes === 0) {
-        return res.status(404).send("Task not found");
-      }
-      return res.json({
-        message: "Task completed",
-      });
-    },
-  );
-});
+router.patch(
+  "/:id/complete",
+  isAuthenticated,
+  validate(idSchema, "params"),
+  validate(completeTaskSchema, "body"),
+  taskControllers.completeTask,
+);
 
 router.patch(
   "/:id/project",
   isAuthenticated,
   validate(idSchema, "params"),
-  validate(idSchema, "body"),
-  moveTask,
+  validate(moveTaskSchema, "body"),
+  taskControllers.moveTask,
 );
+
 export default router;
