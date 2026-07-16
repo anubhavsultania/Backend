0d6c91683fc7a9a12e41bc877fc8b299144f3aa4
anubhavsultania <122CH0539@nitrkl.ac.in>
2026-07-16 23:36:03 +0530

    feat: add DI in controller

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -4,54 +4,62 @@
 //   getProjectsByUserId,
 //   renameProjectWithId,
 // } from "../services/projectServices";
-import { projectService } from "../container.js";
 
-export async function getProjects(req, res, next) {
-  try {
-    const userId = req.session.userId;
-    const result = await projectService.getProjectsByUserId(userId);
-    return res.status(200).json(result);
-  } catch (error) {
-    next(error);
+export function createProjectController(projectService) {
+  export async function getProjects(req, res, next) {
+    try {
+      const userId = req.session.userId;
+      const result = await projectService.getProjectsByUserId(userId);
+      return res.status(200).json(result);
+    } catch (error) {
+      next(error);
+    }
   }
-}
 
-export async function createProject(req, res, next) {
-  try {
-    const userId = req.session.userId;
-    const { title } = req.validatedData.body;
-    const { lastID: newId } = await projectService.createNewProject(
-      userId,
-      title,
-    );
-    return res.status(201).json({
-      id: newId,
-      name: title,
-    });
-  } catch (error) {
-    next(error);
+  export async function createProject(req, res, next) {
+    try {
+      const userId = req.session.userId;
+      const { title } = req.validatedData.body;
+      const { lastID: newId } = await projectService.createNewProject(
+        userId,
+        title,
+      );
+      return res.status(201).json({
+        id: newId,
+        name: title,
+      });
+    } catch (error) {
+      next(error);
+    }
   }
-}
 
-export async function deleteProject(req, res, next) {
-  try {
-    const userId = req.session.userId;
-    const projectId = req.validatedData.params.id;
-    await projectService.deleteProjectById(userId, projectId);
-    return res.sendStatus(204);
-  } catch (error) {
-    next(error);
+  export async function deleteProject(req, res, next) {
+    try {
+      const userId = req.session.userId;
+      const projectId = req.validatedData.params.id;
+      await projectService.deleteProjectById(userId, projectId);
+      return res.sendStatus(204);
+    } catch (error) {
+      next(error);
+    }
   }
-}
 
-export async function renameProject(req, res, next) {
-  try {
-    const userId = req.session.userId;
-    const projectId = req.validatedData.params.id;
-    const { title } = req.validatedData.body;
-    await projectService.renameProjectById(userId, projectId, title);
-    return res.sendStatus(204);
-  } catch (error) {
-    next(error);
+  export async function renameProject(req, res, next) {
+    try {
+      const userId = req.session.userId;
+      const projectId = req.validatedData.params.id;
+      const { title } = req.validatedData.body;
+      await projectService.renameProjectById(userId, projectId, title);
+      return res.sendStatus(204);
+    } catch (error) {
+      next(error);
+    }
   }
+
+  return {
+    getProjects,
+    createProject,
+    deleteProject,
+    renameProject,
+  };
 }
